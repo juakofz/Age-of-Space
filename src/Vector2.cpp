5c56f7825f13da70159cb4d843e69ce2b2640401
juakofz <juakofz@gmail.com>
2017-04-28 20:04:42 +0200

    Updated vector2 class

--- src/Vector2.cpp
+++ src/Vector2.cpp
@@ -8,6 +8,12 @@ Vector2::Vector2(float px, float py)
 	y = py;
 }
 
+Vector2::Vector2(SDL_Point p)
+{
+	x = static_cast<float>(p.x);
+	y = static_cast<float>(p.y);
+}
+
 //Destructor
 Vector2::~Vector2()
 {
@@ -30,15 +36,53 @@ float Vector2::modulo()
 float Vector2::argumento()
 {
 	float aux;
-	aux=180 * atan2(y, x) / M_PI;
-	if(aux<0) return (aux+360);
+	aux = 180 * atan2(y, x) / M_PI;
+	if (aux<0) return (aux + 360);
 	else return aux;
 	//return (180 * atan2(y, x) / M_PI);
+}
+
+Vector2 Vector2::operator + (Vector2 v)
+{
+	return Vector2(x + v.x, y + v.y);
 }
 
 Vector2 Vector2::operator - (Vector2 v)
 {
 	return Vector2(x - v.x, y - v.y);
+}
+
+Vector2 Vector2::operator = (Vector2 v)
+{
+	x = v.x;
+	y = v.y;
+	return *this;
+}
+
+Vector2 Vector2::operator += (Vector2 v)
+{
+	x += v.x;
+	y += v.y;
+	return *this;
+}
+
+Vector2 Vector2::operator -= (Vector2 v)
+{
+	x -= v.x;
+	y -= v.y;
+	return *this;
+}
+
+Vector2 Vector2::operator = (SDL_Point p)
+{
+	x = static_cast<float>(p.x);
+	y = static_cast<float>(p.y);
+	return(Vector2(x, y));
+}
+
+Vector2 Vector2::operator * (float k)
+{
+	return Vector2(k*x, k*y);
 }
 
 float Vector2::distancia(Vector2 dest)
@@ -55,3 +99,26 @@ Vector2 Vector2::norm(float n)
 	aux.y = n * y / modulo();
 	return aux;
 }
+
+Vector2 Vector2::normal(bool right)
+{
+	float arg = argumento();
+	if (right) arg += 90;
+	else arg -= 90;
+	Vector2 aux = direccion(arg);
+	aux = aux * modulo();
+	return aux;
+}
+
+Vector2 Vector2::direccion(float a)
+{
+	Vector2 aux;
+	aux.x = cos(a);
+	aux.y = sin(a);
+	return aux;
+}
+
+//float Vector2::check_angle(float a)
+//{
+//
+//}
--- src/Vector2.h
+++ src/Vector2.h
@@ -1,29 +1,41 @@
 #pragma once
 #include <SDL.h>
+#include <cmath>
 
 class Vector2
 {
-
-
 public:
 
 	//Constructor
 	Vector2(float px = 0, float py = 0);
-	
+	Vector2(SDL_Point p);
+
 	//Destructor
 	~Vector2();
 
 	//Coordenadas
 	float x, y;
 
-	//PAsar a int
+	//Pasar a int
 	SDL_Point convert_int();
 
-	float modulo();
-	float argumento();
-	//void norm(int l);
+	//Operators
 	Vector2 operator - (Vector2 v);
+	Vector2 operator + (Vector2 v);
+	Vector2 operator = (Vector2 v);
+	Vector2 operator += (Vector2 v);
+	Vector2 operator -= (Vector2 v);
+	Vector2 operator = (SDL_Point p);
+
+	Vector2 operator * (float k);
+
 	float distancia(Vector2 dest);
 	Vector2 norm(float n = 1.0);
+	Vector2 normal(bool right = true);
+	float modulo();
+	float argumento();
+
+	static Vector2 direccion(float a);
+//	static float check_angle(float a);
 };
 
